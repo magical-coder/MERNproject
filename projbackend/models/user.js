741ece9dbb7143ed557aee948f1f626af8793eb8
magical-coder <saubhagyasahoo2@gmail.com>
2021-01-13 08:59:30 +0530

    Added new files for the models 1. category --> categorise type of product 2. product  --> description of proucts 3. order    --> for orders in Cart

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
   var Schema  = mongoose.Schema;
 
   var userSchema = new Schema({
@@ -23,9 +26,9 @@ const mongoose = require("mongoose");
         type: String,
         trim: true
     },
-    password: {
+    encry_password: {
         type: String,
-        trim: true
+        required: true
     },
     salt: String,
     role: {
@@ -36,6 +39,34 @@ const mongoose = require("mongoose");
         type: Array,
         default: []
     }
-  });
+  },{timestamps: true});
+
+userSchema.virtual("password")
+  .set(function(password){
+      this._password = password;
+      this.salt = uuidv1();
+      this.encry_password = this.securePassword(password);
+  })
+  .get(function(){
+      return this._password;
+  })
+
+  userSchema.method = {
+      authenticate: function(plainpassword){
+            return this.securePassword(plainpassword) === this.encry_password;
+      },
+      securePassword: function(plainpassword){
+            if(!plainpassword) return "";
+            try{
+
+                return crypto.createHmac('sha256', this.salt)
+                   .update('Plain password')
+                   .digest('hex');
+
+            } catch (err){
+                return "";
+            }
+      }
+  }
 
   module.exports = mongoose.model("User",userSchema)
