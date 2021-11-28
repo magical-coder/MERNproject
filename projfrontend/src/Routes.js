b270c684e603d63d81cccf78ae55ec26f649e50e
magical-coder <saubhagyasahoo2@gmail.com>
2021-11-29 00:56:34 +0530

    started working on the frontend part. Layed a basic react Layout

---
+++ projfrontend/src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Home from './core/Home';
+
+
+const Routes =() => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../styles.css"
+import {API} from "../backend.js"
+import Base from './Base';
+
+
+export default function Home() {
+    console.log("API IS", {API});
+    return (
+        <Base title="Home Page">
+           <div className="row">
+               <div className="col-4">
+                   <button className="btn btn-success">TEST</button>
+               </div>
+               <div className="col-4">
+                   <button className="btn btn-success">TEST</button>
+               </div>
+               <div className="col-4">
+                   <button className="btn btn-success">TEST</button>
+               </div>
+               <div className="col-4"></div>
+               <div className="col-4"></div>
+               <div className="col-4"></div>
+               
+           </div>
+        </Base>
+    )
+}
